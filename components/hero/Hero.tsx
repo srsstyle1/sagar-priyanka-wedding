711ff568392ad38d163a4bc03bbfd0ecdc5345ce
Sagar <Sagar.1.Biswal@coforgetech.com>
2026-06-22 12:20:47 +0530

    Replaced Hero.tss

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -2,11 +2,6 @@
 
 import HeroBackground from "./HeroBackground";
 import HeroContent from "./HeroContent";
-import ScrollIndicator from "./ScrollIndicator";
-
-import Glow from "../effects/Glow";
-import Particles from "../effects/Particles";
-import Vignette from "../effects/Vignette";
 
 export default function Hero() {
   return (
@@ -14,21 +9,11 @@ export default function Hero() {
       id="hero"
       className="relative min-h-screen overflow-hidden"
     >
-      {/* Background */}
       <HeroBackground />
 
-      {/* Visual Effects */}
-      <Vignette />
-      <Glow />
-      <Particles />
-
-      {/* Hero Content */}
       <div className="relative z-20">
         <HeroContent />
       </div>
-
-      {/* Scroll Indicator */}
-      <ScrollIndicator />
     </section>
   );
 }
